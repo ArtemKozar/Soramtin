d034ee94a8627f3a560b50ca8d017ac6bd801d2b
ArtemKozar <kozarart@gmail.com>
2023-05-01 21:12:08 +0300

    connect to firebase

--- frontend_react/src/components/List/List.jsx
+++ frontend_react/src/components/List/List.jsx
@@ -1,20 +1,32 @@
 import {useState} from 'react'
 
+import db from "../../services/firebase"
+import {onSnapshot, collection} from "firebase/firestore"
 import useListAPI from '../../services/useListAPI'
 import ListItem from '../ListItem/ListItem'
 
 import style from './List.module.scss'
 
 import {Pagination} from "@mui/material";
+import {useEffect} from "react";
 
 const List = () => {
     const {list, error} = useListAPI()
     const [userItems, setUserItem] = useState([])
+    const [channels, setChannels] = useState([])
+    console.log(channels)
+
+
+    useEffect(() => {
+        onSnapshot(collection(db, "channels"), (snapshot) =>
+            setChannels(snapshot.docs.map((doc) => ({...doc.data(), id: doc.id})))
+        )
+    }, [])
 
 
     const [pageObject, setPage] = useState({
         page: 1,
-        perPage: 3
+        perPage: 5
     })
 
     const {page, perPage} = pageObject
@@ -25,7 +37,8 @@ const List = () => {
         }))
     }
 
-    const filteredData = list.slice((page - 1) * perPage, page * perPage);
+    // const filteredData = list.slice((page - 1) * perPage, page * perPage);
+    const filteredData = channels.slice((page - 1) * perPage, page * perPage);
 
     if (error) {
         return <h1>Error: {error}</h1>
@@ -49,7 +62,7 @@ const List = () => {
             })}
             <Pagination
                 className={style.pagination}
-                count={Math.ceil(list.length / perPage)}
+                count={Math.ceil(channels.length / perPage)}
                 page={page}
                 onChange={handlerChange}
             />
